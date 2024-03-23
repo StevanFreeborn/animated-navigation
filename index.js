012985938881d8025004fab22392ff2e7b6612c8
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-03-22 23:53:11 -0500

    feat: finish animation for top nav

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
   document
-    .querySelector('#collapseButton')
+    .querySelector('#endUserCollapseButton')
     .addEventListener('click', function() {
       document.querySelector('aside').classList.toggle('collapsed');
+    });
+
+  document
+    .querySelector('#adminCollapseButton')
+    .addEventListener('click', function() {
+      const adminNavigation = document.querySelector('#adminNavigation');
+
+      if (adminNavigation.classList.contains('collapsed')) {
+        adminNavigation.classList.remove('collapsed');
+        adminNavigation.classList.add('expanded');
+        return;
+      }
+      
+      adminNavigation.classList.remove('expanded');
+      adminNavigation.classList.add('collapsed');
     });
 });
